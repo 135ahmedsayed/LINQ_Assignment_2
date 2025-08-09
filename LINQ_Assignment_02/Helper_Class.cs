3e695572ea511a4cd16bc5695a913e643a75144c
135ahmedsayed <ahmedshkra246@gmail.com>
2025-08-09 14:46:16 +0300

    Solve 1.Q1

---
+++ LINQ_Assignment_02/Helper_Class.cs
@@ -0,0 +1,24 @@
+﻿namespace LINQ_Assignment_02;
+
+internal static class Helper_Class
+{
+    public static void print<T>(this IEnumerable<T> list)
+    {
+        if (list == null || !list.Any())
+        {
+            Console.WriteLine("The list is empty or null.");
+            return;
+        }
+        if (list is string)
+        {
+            Console.WriteLine(list);
+        }
+        else
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
--- LINQ_Assignment_02/Program.cs
+++ LINQ_Assignment_02/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace LINQ_Assignment_02
+﻿using static LINQ_Assignment_02.ListGenerators;
+
+namespace LINQ_Assignment_02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region LINQ - Element Operators
+            #region Q1
+            var FirstP = ProductList.First(x => x.UnitsInStock == 0);
+            Console.WriteLine(FirstP);
+            #endregion
+            #endregion
             Console.ReadKey();
         }
     }
