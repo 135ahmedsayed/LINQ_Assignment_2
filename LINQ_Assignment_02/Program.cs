2c2e011c29f36312798207108f6cdd6140a514c2
135ahmedsayed <ahmedshkra246@gmail.com>
2025-08-09 14:03:43 +0300

    Add project files.

---
+++ LINQ_Assignment_02/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LINQ_Assignment_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ReadKey();
+        }
+    }
+}
